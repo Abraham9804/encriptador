679a9f48f438440c1a1163ce0d1591a5e5cc88a1
Abraham Romero <romero9804@hotmail.com>
2024-07-17 17:10:06 -0700

    inicio vista en tablet

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 let textArea = ''
-let messageContainer = ''
-let resultContainer = ''
+const messageContainer = document.querySelector("#message-container")
+const resultContainer = document.querySelector("#result-container")
+const parrafoResultContainer = document.querySelector("#parrafoResult")
+const secctionReseult = document.querySelector("#main__result-section")
 
 function functEncrypt(){
+    //capturar texto del input
     textArea = document.querySelector("#inputText").value
-    console.log(textArea)
     mostrarTexto()
 }
 
 function mostrarTexto(){
-    messageContainer = document.querySelector("#message-container")
-    messageContainer.classList.toggle("disable")
-    resultContainer = document.querySelector("#result-container")
-    resultContainer.innerHTML = "<p>"+textArea+"</p>"
+    //cambiar estilos del contenedor 
+    secctionReseult.style.padding = "64px 16px 40px 16px"
 
+    if(resultContainer.classList.contains("disable")){
+        //Ocultar mensaje 
+        messageContainer.classList.toggle("disable")
+
+        //Mostrar texto ingresado
+        resultContainer.classList.toggle("disable")
+        parrafoResultContainer.innerText = textArea
+        
+    }else{
+        //concatenar texto nuevo ingresado
+        parrafoResultContainer.innerText = textArea
+    }
+    
 }
